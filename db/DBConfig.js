dc4403c86e403fdba9eebbdd9448171d71260827
18255811152 <2206882383@qq.com>
2019-06-01 15:07:16 +0800

    更改端口号

--- db/DBConfig.js
+++ db/DBConfig.js
@@ -1,10 +1,9 @@
-module.exports={
-    mysql:{
-        host:'http://127.0.0.1',
-        user:'root',
-        password:'11111111',
-        database:'nodeExpress',
+module.exports = {
+    mysql: {
+        host: 'http://127.0.0.1',
+        user: 'root',
+        password: '11111111',
+        database: 'nodeExpress',
         port: 3306
     }
-    
 }
