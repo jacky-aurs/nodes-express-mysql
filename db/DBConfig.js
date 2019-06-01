2e4601063277c1f8766a9b6cb948b6cd4306c262
jacky <2206882383@qq.com>
2019-06-01 14:46:16 +0800

    添加注册接口

--- db/DBConfig.js
+++ db/DBConfig.js
@@ -1,9 +1,10 @@
 module.exports={
     mysql:{
-        host:'127.0.0.1',
+        host:'http://127.0.0.1',
         user:'root',
         password:'11111111',
         database:'nodeExpress',
         port: 3306
     }
+    
 }
--- routes/users.js
+++ routes/users.js
@@ -29,51 +29,57 @@ router.get('/', function (req, res, next) {
 });
 
 //添加一个reg作为注册接口
-router.get('/reg', function (req, res, next) {
+// 用户注册
+router.get('/reg',function (req, res, next) {
+  // 从连接池获取连接
   pool.getConnection(function (err, connection) {
-    var param = req.query || req.params;
-    var Username = param.uid;
-    var Psw = param.name;
-    connection.query(usersSQL.queryAll, (err, res)=> {
-      var isTrue = false;
-      if (res) {
-        for (let i = 0; i < res.length; i++) {
-          if (res[i].uid == Username && res[i].name == Psw) {
-            isTrue = true;
+      // 获取前台页面传过来的参数
+      var param = req.query || req.params;
+      var UserName = param.uid;
+      var Password = param.name;
+      var _res = res;
+      connection.query(usersSQL.queryAll, function (err, res) {
+          var isTrue = false;
+          if(res){ //获取用户列表，循环遍历判断当前用户是否存在
+              for (var i=0;i<res.length;i++) {
+                  if(res[i].uid == UserName && res[i].name == Password) {
+                      isTrue = true;
+                  }
+              }
           }
-        }
-      }
-      var data = {};
-      data.isreg = !isTrue;//如果isTrue布尔值为true则登陆成功 有false则失败
-      if (isTrue) {
-        data.result = {
-          code: 1,
-          msg: '用户已存在'
-        };
-      } else {
-        connection.query(usersSQL.insert, [param.uid, param.name], function (err, result) {
-          if (result) {
-            data.result = {
-              code: 200,
-              msg: '注册成功'
-            };
+          var data = {};
+          data.isreg = !isTrue; //如果isTrue布尔值为true则登陆成功 有false则失败
+          if(isTrue) {
+              data.result = {
+                  code: 1,
+                  msg: '用户已存在'
+              };//登录成功返回用户信息
           } else {
-            data.result = {
-              code: -1,
-              msg: '注册失败'
-            };
+              connection.query(usersSQL.insert, [param.uid,param.name], function (err, result) {
+                  if(result) {
+                      data.result = {
+                          code: 200,
+                          msg: '注册成功'
+                      };
+                  } else {
+                      data.result = {
+                          code: -1,
+                          msg: '注册失败'
+                      };
+                  }
+              });
           }
-        });
-      }
-      if(err) data.err=err;
-      //以json形式，把操作结果返回给前台界面
-      setTimeout(()=>{
-        responesJson(_res,data);
-      },300)
-      // 释放链接
-      connection.release();
-    })
-  })
-})
 
+          if(err) data.err = err;
+          // 以json形式，把操作结果返回给前台页面
+          setTimeout(function () {
+            responesJson(_res, data)
+          },300);
+          // responseJSON(_res, data);
+          // 释放链接
+          connection.release();
+
+      });
+  });
+});
 module.exports = router;
